968b1908d0450a64cab80d12ad635ccc3f0dd386
VladislavSafarov <vladsafarov46@gmail.com>
2023-01-18 22:00:56 +0400

    KAM-3: [FE] Application layout template Add Angular Material library.

--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MainComponent } from './main.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { RouterLink } from '@angular/router';
+import { NgForOf } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  imports: [],
+  imports: [MatTabsModule, RouterLink, NgForOf, MatToolbarModule],
   exports: [MainComponent],
   declarations: [MainComponent],
 })
