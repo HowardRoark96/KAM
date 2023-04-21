55f5dc75a812d959901ccf628cc33b75472da74e
VladislavSafarov <vladsafarov46@gmail.com>
2023-04-21 19:04:15 +0400

    KAM-78: [FE] Widget post card component [zn-zorro] Deleted `PostComponent` Made changes after review

--- src/app/pages/main/pages/home/home.module.ts
+++ src/app/pages/main/pages/home/home.module.ts
@@ -2,22 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
-import { PostModule } from '@components/post';
-import { RequestWrapperModule } from '@components/request-wrapper';
-import { ExecuteWithModule } from '@pipes/execute-with';
 import { NewsCardModule } from './widgets/news-card';
 import { CommentModule } from '@widgets/comment';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    HomeRoutingModule,
-    PostModule,
-    RequestWrapperModule,
-    ExecuteWithModule,
-    NewsCardModule,
-    CommentModule,
-  ],
+  imports: [CommonModule, HomeRoutingModule, NewsCardModule, CommentModule],
   declarations: [HomeComponent],
 })
 export class HomeModule {}
